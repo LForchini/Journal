37d602e220b221343b7998011adfa8f41d287f71
Leonardo Forchini <git@lforchini.com>
2022-09-06 11:00:13 +0100

    Changed status code enum

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { StatusCode } from 'status-code-enum';
+import { StatusCodes } from 'http-status-codes';
 import 'dotenv/config';
 import {
     expressjwt,
@@ -31,7 +31,7 @@ app.get('/entries', checkJwt, async (req: JWTRequest, res) => {
     let user_id = req.auth?.sub as string;
     let entries = await Entry.findAll({ where: { user_id } });
 
-    res.status(StatusCode.SuccessOK).send(JSON.stringify(entries));
+    res.status(StatusCodes.OK).send(JSON.stringify(entries));
 });
 
 app.post('/entries', checkJwt, express.json(), async (req: JWTRequest, res) => {
@@ -39,15 +39,15 @@ app.post('/entries', checkJwt, express.json(), async (req: JWTRequest, res) => {
     let user_id = req.auth?.sub as string;
 
     if (!date || !content) {
-        res.sendStatus(StatusCode.ClientErrorBadRequest);
+        res.sendStatus(StatusCodes.BAD_REQUEST);
     } else {
         let entry = await Entry.findOne({ where: { date, user_id } });
         if (entry) {
-            res.sendStatus(StatusCode.ClientErrorBadRequest);
+            res.sendStatus(StatusCodes.BAD_REQUEST);
         } else {
             let entry = new Entry({ date, content, user_id });
             entry.save();
-            res.status(StatusCode.SuccessCreated).send(JSON.stringify(entry));
+            res.status(StatusCodes.CREATED).send(JSON.stringify(entry));
         }
     }
 });
@@ -63,10 +63,10 @@ app.delete(
         let entry = await Entry.findOne({ where: { date, user_id } });
 
         if (!entry) {
-            res.sendStatus(StatusCode.ClientErrorNotFound);
+            res.sendStatus(StatusCodes.NOT_FOUND);
         } else {
             await entry.destroy();
-            res.sendStatus(StatusCode.SuccessNoContent);
+            res.sendStatus(StatusCodes.NO_CONTENT);
         }
     }
 );
@@ -88,7 +88,7 @@ app.patch(
         }
 
         await entry.save();
-        res.sendStatus(StatusCode.SuccessNoContent);
+        res.sendStatus(StatusCodes.NO_CONTENT);
     }
 );
 
